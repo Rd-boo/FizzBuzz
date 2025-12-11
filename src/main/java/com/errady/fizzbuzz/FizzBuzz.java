d5376c2bf2a45f9e038c9807e3a82a1aab0ab5f7
Abderrahmane Er-rady <abdoradiak1234@gmail.com>
2025-12-11 15:14:40 +0100

    final results

--- src/main/java/com/errady/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/errady/fizzbuzz/FizzBuzz.java
@@ -4,14 +4,14 @@ public class FizzBuzz {
 
     public static String de(int n){
 
-        if(n % 3 == 0){
+        if (n % 3 == 0 && n % 5 == 0){  // ToCheck First
+            return "FizzBuzz";
+        }
+        else if(n % 3 == 0){
             return "Fizz";
         }
         else if (n % 5 == 0) {
             return "Buzz";
-        }
-        if (n % 3 == 0 && n % 5 == 0){  // ToCheck First
-            return "FizzBuzz";
         }
         else {
             return Integer.toString(n);
